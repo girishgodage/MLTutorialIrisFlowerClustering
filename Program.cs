7e5bb73f733d12d8390aa14dae5fba09fd7f44b4
girishgodage <girishgodage@gmail.com>
2019-08-01 13:06:39 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// <SnippetUsingsForPaths>
+using System;
+using System.IO;
+// </SnippetUsingsForPaths>
+
+// <SnippetMLUsings>
+using Microsoft.ML;
+using Microsoft.ML.Data;
+// </SnippetMLUsings>
+
+namespace IrisFlowerClustering
+{
+    class Program
+    {
+        // <SnippetPaths>
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+        // </SnippetPaths>
+
+        static void Main(string[] args)
+        {
+            // Create MLContext to be shared across the model creation workflow objects 
+            // Set a random seed for repeatable/deterministic results across multiple trainings.
+            // <SnippetCreateContext>
+            var mlContext = new MLContext(seed: 0);
+            // </SnippetCreateContext>
+
+
+            // STEP 1: Load the data
+           // Loads the training text file into an IDataView  and maps from input columns to IDataView columns.
+            Console.WriteLine($"=============== Loading Dataset  ===============");
+
+            // <SnippetLoadTrainData>
+            IDataView dataView = mlContext.Data.LoadFromTextFile<IrisData>(_dataPath, hasHeader: false, separatorChar: ',');
+            // </SnippetLoadTrainData>
+
+            Console.WriteLine($"=============== Finished Loading Dataset  ===============");
+
+            // STEP 2 - Create a learning pipeline
+            //concatenate loaded columns into one Features column, which is used by a clustering trainer;
+            // use a KMeansTrainer trainer to train the model using the k-means++ clustering algorithm.
+            // <SnippetCreatePipeline>
+            string featuresColumnName = "Features";
+            var pipeline = mlContext.Transforms
+                .Concatenate(featuresColumnName, "SepalLength", "SepalWidth", "PetalLength", "PetalWidth")
+                .Append(mlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+            // </SnippetCreatePipeline>
+
+            // STEP 3 - Train the model
+            // <SnippetTrainModel>
+            var model = pipeline.Fit(dataView);
+            // </SnippetTrainModel>
+
+            // STEP 4 - Save the model 
+            // <SnippetSaveModel>
+            using (var fileStream = new FileStream(_modelPath, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                mlContext.Model.Save(model, dataView.Schema, fileStream);
+            }
+            // </SnippetSaveModel>
+
+            // STEP 5- Use the model for predictions
+            // <SnippetPredictor>
+            var predictor = mlContext.Model.CreatePredictionEngine<IrisData, ClusterPrediction>(model);
+            // </SnippetPredictor>
+
+            // <SnippetPredictionExample>
+            var prediction = predictor.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+            // </SnippetPredictionExample>
+
+
+
+        }
+    }
+}
